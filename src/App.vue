0c31d6986c299d437c1b05c1305ee2a82993a030
Lionel COTE <grcote7@gmail.com>
2023-05-23 18:39:17 +0200

    Feat: Dynamic computed v-model

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,22 @@
 <template>
-	<HelloWorld />
-	<div class="bold" :class="[{ red: isMineur, green: !isMineur }]">
-		{{ user.name }}
-	</div>
+  <HelloWorld />
+  <span>{{ sentence }}</span>
+  
+  <input type="text" v-model="name">
+  <input type="text" v-model="age">
 </template>
 
 <script setup>
 import HelloWorld from "@/components/HelloWorld.vue";
-import { reactive } from "vue";
+import { computed, ref } from "vue";
 
-const user = reactive({
-	name: "GrCOTE7",
-	age: 17,
+const name = ref('GrCOTE7')
+const age = ref(17);
+
+const sentence = computed(()=>{
+  return `Bonjour, je m'appelle ${name.value} et j'ai ${age.value} ans.`;
 });
-
-const isMineur = user.age < 18;
-
-const display = () => {
-	console.log(user.name, user.age);
-};
-
-display();
+// console.log(sentence.value)
 </script>
 
 <style scoped>
