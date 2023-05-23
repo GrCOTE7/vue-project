5ee2d10eb0a64e74034a84b7afdcb5d20eee3d85
Lionel COTE <grcote7@gmail.com>
2023-05-23 18:43:34 +0200

    Feat: Watch()

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,20 @@
 <template>
-  <HelloWorld />
-  <span>{{ sentence }}</span>
-  
-  <input type="text" v-model="name">
-  <input type="text" v-model="age">
+	<HelloWorld />
+
+	<input type="text" v-model="name" />
+	<input type="text" v-model="age" />
 </template>
 
 <script setup>
 import HelloWorld from "@/components/HelloWorld.vue";
-import { computed, ref } from "vue";
+import { watch, ref } from "vue";
 
-const name = ref('GrCOTE7')
+const name = ref("GrCOTE7");
 const age = ref(17);
 
-const sentence = computed(()=>{
-  return `Bonjour, je m'appelle ${name.value} et j'ai ${age.value} ans.`;
+watch(name, (newValue, oldValue) => {
+	console.log(newValue, oldValue);
 });
-// console.log(sentence.value)
 </script>
 
 <style scoped>
